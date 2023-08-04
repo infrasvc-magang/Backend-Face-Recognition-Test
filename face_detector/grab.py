3320590485081cb3957e49022933fa0b325aad5f
unknown <ridhowu@student.telkomuniversity.ac.id>
2023-08-04 15:18:58 +0700

    Penyempurnaan Backend Face Recognition

--- face_detector/grab.py
+++ face_detector/grab.py
@@ -1,30 +1,18 @@
-# import the necessary packages
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
-import numpy as np
-import urllib
-import json
 import cv2
-import base64
+import urllib
+import numpy as np
+
 
 def _grab_image(path=None, stream=None, url=None):
-	# if the path is not None, then load the image from disk
-	if path is not None:
-		image = cv2.imread(path)
-	# otherwise, the image does not reside on disk
-	else:	
-		# if the URL is not None, then download the image
-		if url is not None:
-			resp = urllib.urlopen(url)
-			data = resp.read()
-		# if the stream is not None, then the image has been uploaded
-		elif stream is not None:
-			data = stream.read()
+    if path is not None:
+        image = cv2.imread(path)
+    else:
+        if url is not None:
+            resp = urllib.urlopen(url)
+            data = resp.read()
+        elif stream is not None:
+            data = stream.read()
+        image = np.asarray(bytearray(data), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-		# convert the image to a NumPy array and then read it into
-		# OpenCV format
-		image = np.asarray(bytearray(data), dtype="uint8")
-		image = cv2.imdecode(image, cv2.IMREAD_COLOR)
- 
-	# return the image
-	return image
+    return image
--- face_detector/models.py
+++ face_detector/models.py
@@ -1,40 +1,11 @@
-import face_recognition as fr
-import math
-import os
+import numpy as np
 from keras.models import load_model
 
-age_dir = 'face_detector/model/agegender.h5'
-emotion_dir = 'face_detector/model/emotion.h5'
-emotion_model = load_model(emotion_dir)
-age_model = load_model(age_dir)
-emotion_dict = {0: "Angry", 1: "Happy", 2: "Disgust",
-                3: "Surprise", 4: "Sad", 5: "Fear", 6: "Neutral"}
 
-face_locations = []
-face_encodings = []
-face_names = []
-known_face_encodings = []
-known_face_names = []
+model_age = load_model('face_detector/model/agegender.h5')
+model_emotion = load_model('face_detector/model/emotion.h5')
 
-def face_confidence(face_distance, face_match_threshold=0.6):
-    range = (1.0 - face_match_threshold)
-    linear_val = (1.0 - face_distance) / (range * 2.0)
 
-    if face_distance > face_match_threshold:
-        return str(round(linear_val * 100, 2)) + '%'
-    else:
-        value = (linear_val + ((1.0 - linear_val) *
-                 math.pow((linear_val - 0.5) * 2, 0.2))) * 100
-        return str(round(value, 2)) + '%'
-
-def encode_faces():
-	for image in os.listdir('images'):
-		face_image = fr.load_image_file(f'images/{image}')
-		face_encoding = fr.face_encodings(face_image)[0]
-
-		known_face_encodings.append(face_encoding)
-		known_face_names.append(image)
-                
 def get_age(distr):
     if distr >= 1 and distr <= 10:
         return "9-18"
@@ -46,7 +17,7 @@ def get_age(distr):
         return "38-49"
     if distr >= 60:
         return "60 +"
-    return "Unknown"
+    return 'Unknown'
 
 
 def get_gender(prob):
@@ -54,3 +25,12 @@ def get_gender(prob):
         return "Male"
     else:
         return "Female"
+
+
+def get_emotion(hrr):
+    emotion_dict = {0: "Angry", 1: "Happy", 2: "Disgust",
+                    3: "Surprise", 4: "Sad", 5: "Fear", 6: "Neutral"
+                    }
+
+    maxindex = int(np.argmax(hrr))
+    return emotion_dict[maxindex]
